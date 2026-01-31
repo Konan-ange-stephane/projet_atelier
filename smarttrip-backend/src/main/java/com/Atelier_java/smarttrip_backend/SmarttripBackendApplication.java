78f84d48ce4232d3b18366eccb2d4503b306aef3
Konan-ange-stephane <konanangestephane3@gmail.com>
2026-01-31 00:03:55 +0000

    init backend

---
+++ smarttrip-backend/src/main/java/com/Atelier_java/smarttrip_backend/SmarttripBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Atelier_java.smarttrip_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarttripBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarttripBackendApplication.class, args);
+	}
+
+}
---
+++ smarttrip-backend/src/test/java/com/Atelier_java/smarttrip_backend/SmarttripBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Atelier_java.smarttrip_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarttripBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
